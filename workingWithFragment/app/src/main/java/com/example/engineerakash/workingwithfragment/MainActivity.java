4031c673c84c25bd8cc413c51de5110e2ff4bc0a
akash-kumar-basak <akash4116@gmail.com>
2017-01-20 13:33:47 -0800

    fargment work start

---
+++ workingWithFragment/app/src/main/java/com/example/engineerakash/workingwithfragment/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.engineerakash.workingwithfragment;
+
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        StartFragment startFragment = new StartFragment();
+        fragmentTransaction.add(R.id.fragmentBase, startFragment);
+        fragmentTransaction.commit();
+    }
+}
---
+++ workingWithFragment/app/src/main/java/com/example/engineerakash/workingwithfragment/StartFragment.java
@@ -0,0 +1,22 @@
+package com.example.engineerakash.workingwithfragment;
+
+import android.app.Fragment;
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+public class StartFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_start, container, false);
+    }
+
+}
